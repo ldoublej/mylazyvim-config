098adecc09fe032f9b9589a6e010528de4f7e6f4
ldoublej <1208900797@qq.com>
2026-03-08 17:58:10 +0800

    feat: cmake support & cmake keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local wk = require("which-key")
+
+-- cmake tools
+map("n", "<leader>cmg", "<cmd>CMakeGenerate<cr>", { desc = "Cmake Generate" })
+map("n", "<leader>cmb", "<cmd>CMakeBuild<cr>", { desc = "Cmake Build" })
+map("n", "<leader>cmc", "<cmd>CMakeClean<cr>", { desc = "Cmake Clean" })
+wk.add({
+    { "<leader>cm", "CMake", mode = { "n" } },
+})
